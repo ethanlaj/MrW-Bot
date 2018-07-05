eb3b796772724c76079377fe21f80d02fe04c9e7
ethanlaj <ethanlaj@ericlaj.com>
2018-07-04 21:53:00 -0400

    time command

--- load/databaseupdater.js
+++ load/databaseupdater.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot) => {
 	channels = bot.guilds.get("443929284411654144").channels.filter((m) => RegExp("wbotprefixes-database", "gi").test(m.name));
 	var nestedMessages = await Promise.all(channels.map((ch) => ch.fetchMessages({ limit: 100 })));
 	var flatMessages = nestedMessages.reduce((a, b) => a.concat(b));
-	for (let msg of flatMessages) {
+	for (let msg of flatMessages.array()) {
 		var guild = msg.content.split(" ")[0];
 		var prefix = msg.content.split(" ")[1];
 		bot.databases.prefixes.push({ guild: guild, prefix: prefix });
@@ -11,7 +11,7 @@ module.exports.run = async (bot) => {
 	channels = bot.guilds.get("443929284411654144").channels.filter((m) => RegExp("wbotdisable-database", "gi").test(m.name));
 	nestedMessages = await Promise.all(channels.map((ch) => ch.fetchMessages({ limit: 100 })));
 	flatMessages = nestedMessages.reduce((a, b) => a.concat(b));
-	for (let msg of flatMessages) {
+	for (let msg of flatMessages.array()) {
 		var commands = msg.content.split(" ");
 		guild = commands.shift();
 		bot.databases.disabled.push({ guild: guild, commands: commands });
