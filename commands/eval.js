21d5434d17f73cb53db58c53b3e53ba4f45893d4
ethanlaj <ethanlaj@ericlaj.com>
2018-07-15 12:34:37 -0400

    fix

--- commands/eval.js
+++ commands/eval.js
@@ -1,48 +1,48 @@
 const { RichEmbed } = require("discord.js");
 function clean(text, token) {
-    if (typeof(text) === "string") {
-        return text.replace(/`/g, "`" + String.fromCharCode(8203))
-            .replace(/@/g, "@" + String.fromCharCode(8203))
-            .replace(new RegExp(token, "gi"), "[ HIDDEN ]");
-    } else {
-        return text;
-    }
+	if (typeof (text) === "string") {
+		return text.replace(/`/g, "`" + String.fromCharCode(8203))
+			.replace(/@/g, "@" + String.fromCharCode(8203))
+			.replace(new RegExp(token, "gi"), "[ HIDDEN ]");
+	} else {
+		return text;
+	}
 }
 
 var allowed = ["399975738008141824"];
 module.exports.run = async (bot, message, args) => {
-    if (allowed.includes(message.author.id)) {
-        try {
-            var code = args.join(" ");
-            var currentTime = Date.now();
-            var evaled = await eval(code);
-            if (typeof evaled !== "string")
-                evaled = require("util").inspect(evaled);
-            message.channel.send({
-                embed: new RichEmbed()
-                    .setTitle("Evaled")
-                    .setDescription(`\`\`\`js\n${clean(evaled, bot.token).substring(0, 500)}\`\`\``)
-                    .setFooter(`Took ${Date.now() - currentTime} milliseconds.`)
-                    .setColor("GREEN")
-            });
-        } catch (err) {
-            message.channel.send({
-                embed: new RichEmbed()
-                    .setTitle("Error")
-                    .setDescription(`\`\`\`x1\n${clean(err, bot.token)}\`\`\``)
-                    .setFooter(`Took ${Date.now() - currentTime} milliseconds.`)
-                    .setColor("RED")
-            });
-        }
-    } else {
-        message.channel.send({
-            embed: new RichEmbed()
-                .setTitle("Access Denied")
-                .setDescription("x1\nYou don't have permission to use this command.")
-                .setColor("RED")
-        });
-    }
+	if (allowed.includes(message.author.id)) {
+		try {
+			var code = args.join(" ");
+			var currentTime = Date.now();
+			var evaled = await eval(code);
+			if (typeof evaled !== "string")
+				evaled = require("util").inspect(evaled);
+			message.channel.send({
+				embed: new RichEmbed()
+					.setTitle("Evaled")
+					.setDescription(`\`\`\`js\n${clean(evaled, bot.token).substring(0, 500)}\`\`\``)
+					.setFooter(`Took ${Date.now() - currentTime} milliseconds.`)
+					.setColor("GREEN")
+			});
+		} catch (err) {
+			message.channel.send({
+				embed: new RichEmbed()
+					.setTitle("Error")
+					.setDescription(`\`\`\`x1\n${clean(err, bot.token)}\`\`\``)
+					.setFooter(`Took ${Date.now() - currentTime} milliseconds.`)
+					.setColor("RED")
+			});
+		}
+	} else {
+		message.channel.send({
+			embed: new RichEmbed()
+				.setTitle("Access Denied")
+				.setDescription("x1\nYou don't have permission to use this command.")
+				.setColor("RED")
+		});
+	}
 };
 module.exports.help = {
-    name: "eval"
+	name: "eval"
 };
