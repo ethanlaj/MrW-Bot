a95b682b40487a980b6a6b5bea6b199b926ece67
ethanlaj <ethanlaj@ericlaj.com>
2018-07-16 10:01:05 -0400

    purge 100

--- commands/purge.js
+++ commands/purge.js
@@ -3,10 +3,13 @@ module.exports.run = async (bot, message, args) => {
 	if (message.member.hasPermission("MANAGE_MESSAGES")) {
 		let num = parseInt(args[0]);
 		if (!num) return message.reply("You must provide the number of messages to delete!");
-		if (num > 99) return message.reply("You can only purge 99 messages at a time!");
-		message.channel.bulkDelete(num + 1)
-			.then((messages) => message.reply(`Deleted ${messages.size - 1} messages that were not over two weeks old!`).then((msg) => msg.delete(5000)))
-			.catch(console.error);
+		if (num > 100) return message.reply("You can only purge 100 messages at a time!");
+		message.channel.bulkDelete(num).then((messages) => {
+			message.reply(`Deleted ${messages.size} messages that were not over two weeks old!`).then((msg) => {
+				msg.delete(5000);
+			});
+			message.delete();
+		});
 		var logsDatabase = bot.channels.find("id", "443931379907166210");
 		logsDatabase.fetchMessages({ limit: 100 }).then((logmessages) => {
 			for (let msg of logmessages.array()) {
