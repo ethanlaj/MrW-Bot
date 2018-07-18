5b7856e3d1a60bf60676a296d797d5a1d930fdea
Yikey <gtc122004@gmail.com>
2018-07-18 16:30:25 -0400

    Create weather.js

---
+++ commands/weather.js
@@ -0,0 +1,33 @@
+const { RichEmbed } = require("discord.js");
+const weather = require("weather-js");
+
+module.exports = {
+	help: {
+		name: "weather",
+		description: "Displays the weather in the area given",
+		aliases: ["w"],
+		category: "Information
+	},
+	run: async (call) => {
+		var area = call.params.readParam(true);
+		if (area) {
+			weather.find({ search: area, degreeType: "F" }, (err, result) => {
+				if (err) console.warn(err.stack);
+				result = result[0];
+				if (result) {
+					var weatherEmbed = new RichEmbed()
+						.setTitle(`Weather in ${result.location.name}`)
+						.addField("Temperature", `${result.current.temperature} °F.`, true)
+						.addField("Feels Like", `${result.current.feelslike} °F.`, true)
+						.addField("Humidity", `${result.current.humidity}%`, true)
+						.addField("Sky", `${result.current.skytext}.`, true)
+						.addField("Last Updated", result.current.observationtime, true)
+						.addField("Windspeed", `${result.current.windspeed}.`, true)
+						.setColor(0x00AE86)
+						.setDefaultFooter(call.message.author);
+					call.safeSend(null, call.message, { embed: weatherEmbed });
+				} else call.safeSend("Could not find the area given.");
+			});
+		} else call.safeSend("Please specify an area to view the weather in.");
+	}
+};
