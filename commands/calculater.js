9cd4d9d1c61ef5bfef4944d69354253462b5b2da
Yikey <gtc122004@gmail.com>
2018-07-18 20:58:50 -0400

    Create calculater.js

---
+++ commands/calculater.js
@@ -0,0 +1,29 @@
+const { RichEmbed } = require("discord.js");
+const math = require("mathjs");
+
+module.exports = {
+	help: {
+    name: "calculator",
+    aliases: ["math", "calc", "c"],
+    description: "Allows you to run math equations"
+  },
+	run: async (_1, message, _2, _3, content) => {
+		var expression = content;
+		var mathEmbed = new RichEmbed();
+		if (expression) {
+			try {
+				var result = math.eval(expression);
+				mathEmbed
+					.setTitle("Success")
+					.setDescription(`Result:\n\`\`\`js\n${result}${" ".repeat(3)}\`\`\``)
+					.setColor("GREEN");
+			} catch (exc) {
+				mathEmbed
+					.setTitle("Error")
+					.setDescription(`Error while parsing expression supplied: \`${exc.message.replace(/`/g, "")}\`.`)
+					.setColor("RED");
+			}
+			message.channel.send({ embed: mathEmbed });
+		} else message.reply("You must supply an expression.");
+	}
+};
