1aa1d512f546ee4a01bbf4bca143c5228e7690f4
Windows <36823560+mrwagner27@users.noreply.github.com>
2019-01-20 10:50:16 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message) => {
-	message.reply(`**${bot.user.username}'s support guild:** https://discord.gg/UC37qGN`);
+	message.reply(`**${bot.user.username}'s support guild:** This Is Coming Soon Sorry`);
 };
 module.exports.help = {
 	name: "server",
